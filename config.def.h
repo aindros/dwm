343a754ede81c98e97b1afbb0697ecf2638fd4ff
Alessandro Iezzi <aiezzi@alessandroiezzi.i>
2022-02-06 17:05:13 +0100

    Add Pidgin to ruels

--- config.def.h
+++ config.def.h
@@ -29,6 +29,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Pidgin",   NULL,       NULL,       1 << 8,       1,           -1 },
 };
 
 /* layout(s) */
@@ -59,7 +60,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "mate-terminal", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
